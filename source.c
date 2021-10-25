1ab9f319b301e710d95e60414333249aa9ef71c7
Дмитрий Туминов <d.tuminov1@outlook.com>
2021-10-25 23:28:50 +0300

    new

--- source.c
+++ source.c
@@ -4,17 +4,32 @@
 #include <string.h>
 
 int sortFile(char * filePath){
-    char symbol;
+    char symbol = ' ';
     FILE *file = fopen(filePath, "r");
-    char string [200];
-    //char **arr = (char**)malloc(length*sizeof(char*));
-    int i = 0;
-    while ((symbol = getc(file)) != EOF)
-    {
-        string[i] = symbol;
-        i++;
+    char string[200] = "";
+    char **arr = calloc(10, sizeof(char*)) ;
+    size_t i = 0;
+    int lines_count = 1;
+    while (! feof(file)){
+        if (fgetc(file) == '\n'){
+            lines_count++;
+        }
+    } 
+    printf("%d\n",lines_count);
+    fseek(file, 0, SEEK_SET);
+    for (; i < lines_count; i++) {
+        for (size_t j = 0; (symbol = fgetc(file)) != EOF; j++){
+            printf("%c",symbol);
+            if (symbol == '\n') {
+                j = 0; 
+            }
+            //сука 27 строка бл 
+            //arr[i][j] = symbol;
+        }
     }
-    printf("%s",string);
-    
+    for (size_t j = 0; j < i; j++)
+    {
+        printf("\n%s", arr[j]);
+    }
     fclose(file);
 }
